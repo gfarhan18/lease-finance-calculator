08cd621f2ec75c5f54c2462fa7f9fe907ded7f92
Asiya <asiya.batool987@gmail.com>
2024-02-17 20:05:03 +0500

    fixed header responsiveness

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
 
-// components/Header.tsx
 const Header: React.FC = () => {
   return (
     <div className="bg-black shadow-md">
-      <div className="container mx-auto px-4 py-6 flex justify-between items-center">
-        <Image src="/Dealer-Tactics-Logo.png" alt="Logo" width={100} height={50} />
-        <h1 className="text-3xl bg-clip-text text-transparent bg-gradient-to-r from-yellow-400 to-gray-300 px-2 font-bold">Lease Payment Calculator and Finance Calculator</h1>
-
-        <a href="tel:+1234567890" className="text-white hover:text-yellow-200">
+      <div className="container mx-auto px-4 py-3 md:py-6 flex flex-col md:flex-row justify-between items-center">
+          <Image src="/Dealer-Tactics-Logo.png" alt="Logo" width={150} height={50} />
+          <h1 className="text-2xl md:text-3xl bg-clip-text text-transparent bg-gradient-to-r from-yellow-400 to-gray-300 p-2 font-bold text-center">
+            Lease Payment Calculator and Finance Calculator
+          </h1>
+        <a href="tel:+1234567890" className="text-white hover:text-yellow-200 mt-2 md:mt-0">
           +1 (234) 567-890
         </a>
       </div>
